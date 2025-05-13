c1f19008f55cd2df1da4fef1ffd82ca15e65daca
Carl <carl.pattersonsd@gmail.com>
2025-05-13 22:48:17 +0000

    update streamlit requirments streamlit

---
+++ stream-lit.py
@@ -0,0 +1,26 @@
+
+# create a sidebar that includes 
+# input file
+# input file name
+# input player description
+# run the agent
+# print the report
+# save the report with the input name
+
+import streamlit as st
+
+buffer, col2, col3 = st.columns([1, 20, 60])
+
+with col2:
+    player = st.sidebar.text_input("file name"),
+    image = st.sidebar.file_uploader("Choose a file", accept_multiple_files=True)
+
+with col3:
+    Markdown(results)
+
+try:
+    doc = Document()
+    doc_para = doc.add_paragraph(results)
+    doc.save('cleaned_doc.docx')
+except Exception as e:
+    print(f"An error occurred: {e}")
